9d96b8f61b86bddc4df40dc03d580d0d0c9ae1a2
kwon <ygkwon50@gmail.com>
2020-11-24 01:05:08 +0900

    update menu

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -2,6 +2,7 @@ package com.example;
 
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import java.util.Scanner;
 
 
 public class Main {
@@ -12,10 +13,12 @@ public class Main {
         final AmazonEC2 ec2= AmazonEC2ClientBuilder.defaultClient();
         
         int menu_num;
+        String instance_id;
+        
         
         Scanner num = new Scanner(System.in);
+        Scanner id = new Scanner(System.in);
         
-
         while(true)
         {
             System.out.println("-----------------------------------------------------");
@@ -33,17 +36,57 @@ public class Main {
 
             System.out.println("-----------------------------------------------------");
             
+            System.out.println("Put menu's number");
+            menu_num=num.nextInt();
+            
+            if(menu_num==1)
+            {
+            	System.out.println("Put your instance Id");
+            	instance_id=id.nextLine();
+            	
+            	//StartInstance.startIns(ec2,instance_id);
+            	
+            	StartInstance.startIns(ec2,"i-077c7e78bf16a3391");
+            }
             
             
-        	
+            else if(menu_num==2)
+            {
+            	
+            }
+            else if(menu_num==3)
+            {
+            	
+            }
+            else if(menu_num==4)
+            {
+            	
+            }
+            else if(menu_num==5)
+            {
+            	
+            }
+            else if(menu_num==6)
+            {
+            	
+            }
+            else if(menu_num==7)
+            {
+            	
+            }
+            else if(menu_num==8)
+            {
+            	
+            }
+            else if(menu_num==9)
+            {
+            	return;
+            }
+            
         }
         
         
         
-        StartInstance.startIns(ec2,"i-077c7e78bf16a3391");
-        
-        
-        
-
+     
     }
 }
